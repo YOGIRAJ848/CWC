f4d33a97f3349aadf240e6bba232da74e8aa916a
Paradox7 <yogirajnagur510@gmail.com>
2025-08-24 19:31:30 +0530

    add bot.js

---
+++ src/bot.js
@@ -0,0 +1,28 @@
+import { Telegraf } from "telegraf";
+const bot = new Telegraf(process.env.8183297173:AAHS36fw0KgZtztJm6x1-JBvIej5tKI8Ph0)
+
+// Handle /start
+bot.start((ctx) => {
+ctx.reply("🚀 Welcome to CWC WebApp! Click below to open:", {
+reply_markup: {
+inline_keyboard: [
+[
+{
+text: "Open App",
+web_app: { url: "https://cwc-git-main-paradox-s-projects-703097cc.vercel.app?_vercel_share=0JMqBOjFmCNTlSB3Rbzx1EXNU639HW7o" }
+}
+]
+]
+} 
+});
+});
+
+export default async function handler(req, res) {
+try {
+await bot.handleUpdate(req.body);
+res.status(200).json({ ok: true });
+} catch (err) {
+console.error(err);
+res.status(500).send("Bot error");
+}
+}
